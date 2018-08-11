cc0d079f6a459299394f728a51febca42ce791e0
CMOW5 <tec.cmos@gmail.com>
2018-08-11 17:48:08 -0500

    js prototype

---
+++ prototype/class-sugar.js
@@ -0,0 +1,26 @@
+class Foo {
+   constructor(who) {
+    this.me = who;
+   }
+  
+  identify() {
+    return "I am " + this.me;
+  }
+}
+
+
+class Bar extends Foo {
+  constructor(who) {
+    super(who);
+  }
+
+  speak() {
+    console.log( "Hello, " + super.identify() + "." );
+  }
+}
+
+var b1 = new Bar("b1");
+var b2 = new Bar("b2");
+
+b1.speak();
+b2.speak();
